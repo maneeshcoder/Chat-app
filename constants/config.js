5cc9541212200b106fe5183aa7b793707baff228
maneeshcoder <maneeshrawat143@gmail.com>
2025-02-05 16:22:08 +0530

    rsolved cors issue

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:4173",
+    "https://chat-app-clinet.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
